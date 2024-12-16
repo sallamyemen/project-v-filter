e68d10aac9cc51d03a2fa532bef6bfaf6b03fc6a
sallamyemen <sallamyemen@hotmail.com>
2024-12-16 23:32:20 +0300

    app.vue updated

--- app.vue
+++ app.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+
+
+<script setup>
+
+// import {useProductsStore} from "./stores/products";
+// import {useFiltersStore} from "./stores/filters";
+
+// const productsStore = useProductsStore();
+// const filtersStore = useFiltersStore();
+
+</script>
